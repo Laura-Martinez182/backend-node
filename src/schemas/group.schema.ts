6be5dcfe487ed77b0144ef15f2be7fc1307bd5f8
JuanF2019 <juan.martinez27@u.icesi.edu.co>
2023-11-15 21:06:20 -0500

    Add role validation, translated error messages to English

--- src/schemas/group.schema.ts
+++ src/schemas/group.schema.ts
@@ -1,5 +1,5 @@
 import { z } from 'zod';
 
 export const groupSchema = z.object({
-    name: z.string().min(5).max(255),
+    name: z.string().min(5,"Group name must be at least 5 characters long").max(255,"Group name must be shorter than 255 characters"),
 });
--- src/schemas/user.schema.ts
+++ src/schemas/user.schema.ts
@@ -1,9 +1,14 @@
 import { z } from 'zod';
 
+const roles = ["superadmin", "user"]
+
 export const userSchema = z.object({
-    name: z.string().min(5).max(255),
+    name: z.string().min(5,"Name must be at least 5 characters long").max(255,"Name must be shorter than 255 characters"),
     email: z.string().email({
-        message: "Correo electronico invalido",
+        message: "Invalid email",
     }),
-    password: z.string().min(6, "Contraseña débil")
+    password: z.string().min(6, "Weak password"),
+    role: z.string().refine((role) => roles.includes(role),{
+        message:'Role must be one the following: ' + roles.join(', '),
+    })
 });
